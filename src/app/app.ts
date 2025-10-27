e909f7661617404da1a5042258ef5956100a3408
Spokay <hugobressange@gmail.com>
2025-10-27 19:26:49 +0100

    feat: added basic layout to the template with login / logout buttons

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,13 @@ import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { OidcSecurityService } from 'angular-auth-oidc-client';
 
+import { Footer } from './layout/footer/footer';
+import { Header } from './layout/header/header';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.html',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer],
 })
 export class App implements OnInit {
   private readonly oidcSecurityService = inject(OidcSecurityService);
---
+++ src/app/layout/header/header.ts
@@ -0,0 +1,24 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OidcSecurityService } from 'angular-auth-oidc-client';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.html',
+  styleUrls: ['./header.css'],
+  imports: [CommonModule],
+})
+export class Header {
+  private readonly oidcSecurityService = inject(OidcSecurityService);
+
+  isAuthenticated$ = this.oidcSecurityService.isAuthenticated$;
+  userData$ = this.oidcSecurityService.userData$;
+
+  login(): void {
+    this.oidcSecurityService.authorize();
+  }
+
+  logout(): void {
+    this.oidcSecurityService.logoff().subscribe();
+  }
+}
