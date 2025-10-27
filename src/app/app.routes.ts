f1e02dfe6de2b296b420334a6ef3e9225fbd2067
Spokay <hugobressange@gmail.com>
2025-10-27 17:25:26 +0100

    fix: fixing conflicts after rebase

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { AutoLoginPartialRoutesGuard } from 'angular-auth-oidc-client';
 
-import { Home } from './home/home';
+import { Home } from './components/home/home';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
